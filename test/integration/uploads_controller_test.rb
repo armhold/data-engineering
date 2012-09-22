f1dbce1274cbeba45a79de0b98c4661729447d6b
George Armhold <armhold@gmail.com>
2012-09-22 18:16:19 -0400

    integration tests

---
+++ test/integration/uploads_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UploadsControllerTest < ActionController::TestCase
+
+  test "cannot access uploads page when not logged in" do
+    get :index
+
+    assert_redirected_to new_session_path
+  end
+
+  test "can access uploads page after login" do
+    user = users(:one)
+    @request.session[:user_id] = user
+
+    get :index
+
+    assert_equal "200", response.code
+    assert_select "div#upload-page", { :count => 1}
+    assert_select "h2#welcome", { :count => 1, :text => "Welcome, John [ Logout ]" }
+  end
+
+
+
+end
